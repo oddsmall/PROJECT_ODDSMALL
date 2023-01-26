8b9365cfd066cf88ce1a382afd68914fcdb6526e
kkkukkk <jin960828@naver.com>
2023-01-26 21:16:44 +0900

    프로젝트 생성

---
+++ oddsmall/src/main/java/com/example/oddsmall/OddsmallApplication.java
@@ -0,0 +1,13 @@
+package com.example.oddsmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OddsmallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OddsmallApplication.class, args);
+    }
+
+}
